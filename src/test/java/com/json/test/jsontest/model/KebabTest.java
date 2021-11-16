975abdc168909850ca66236b067923f04faf221f
username-Hello <nikitos.burak@gmail.com>
2021-11-16 03:20:41 +0300

    KEBAB json naming strategies test

---
+++ src/test/java/com/json/test/jsontest/model/KebabTest.java
@@ -0,0 +1,25 @@
+package com.json.test.jsontest.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class KebabTest extends BaseTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void kebabTest() throws JsonProcessingException {
+        BeerDto beerDto = getBeerDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+}
